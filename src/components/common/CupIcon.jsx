c682ac5c3fc71588bc91750fb1c46ba58efae3e9
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2023-01-15 20:47:56 -0800

    replace image tag with next/image

--- src/components/common/CupIcon.jsx
+++ src/components/common/CupIcon.jsx
@@ -1,18 +1,24 @@
-const CupIcon = ({src, alt}) => (
-        <img
+import Image from "next/image";
+
+const CupIcon = ({src, alt, width, height}) => (
+        <Image
             src={src}
             alt={alt}
+            width={width}
+            height={height}
         />
 );
 
-const CupIconContainer = ({food}) => {
-    const src = food === "" ? "icons/cup.png": `icons/cup-${food}.png`;
+const CupIconContainer = ({food, width, height}) => {
+    const src = food === "" ? "/icons/cup.png": `/icons/cup-${food}.png`;
     const alt = food === "" ? "cup icon" : `${food} cup icon`;
 
     return (
         <CupIcon
             src={src}
             alt={alt}
+            width={width || 100}
+            height={height || 100}
         />
     );
 };
