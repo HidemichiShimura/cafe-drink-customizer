d73bc37c704d284fef7eced59fd472e943ef500a
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2023-01-17 12:38:18 -0800

    create a reusable component Banner

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,8 @@
  * Common
  */
 
-export { default as CupIcon } from "./common/CupIcon";
-export { default as MoodIcon } from "./common/MoodIcon";
+export { default as CupIcon } from './common/CupIcon'
+export { default as MoodIcon } from './common/MoodIcon'
+
+export { default as Banner } from './common/banner/Banner'
+export { default as BannerContent } from './common/banner/BannerContent'
