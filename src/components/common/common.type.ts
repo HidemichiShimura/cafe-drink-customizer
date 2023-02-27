77ff4ed97d0e89527274d9e73c06f03923470736
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2023-02-27 15:27:13 -0800

    create a type file for props commonly used in multiple common components

---
+++ src/components/common/common.type.ts
@@ -0,0 +1,13 @@
+export interface IconProps {
+  src: string
+  alt: string
+  width: number
+  height: number
+}
+
+export interface IconContainerProps {
+  food: string
+  mood: string
+  width: number
+  height: number
+}
