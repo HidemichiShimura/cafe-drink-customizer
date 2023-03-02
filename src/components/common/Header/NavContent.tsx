2afe5ed84bedde850bfaf1fe50fd197a5f75a889
Yuya Minamide <dettu1003@gmail.com>
2023-03-01 20:55:48 -0800

    added sign out button on header

--- src/components/common/Header/NavContent.tsx
+++ src/components/common/Header/NavContent.tsx
@@ -1,24 +1,58 @@
-import { FC } from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { FC, useState } from 'react'
+import { auth } from 'hooks/firebase'
 import styles from 'styles/common/Header/NavContent.module.scss'
 
 const NAV_ITEMS: string[] = ['About']
-const NavContent: FC = () => (
-  <>
-    {NAV_ITEMS.map((item: string, idx: number) => (
+const NavContent: FC = () => {
+  const router = useRouter()
+  const [error, setError] = useState<string | null>(null)
+
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        router.push('/login')
+      })
+      .catch((error) => {
+        setError(error.message)
+      })
+  }
+  return (
+    <>
+      {NAV_ITEMS.map((item: string, idx: number) => (
+        <li
+          className={styles['nav-content']}
+          key={idx}
+        >
+          <Link
+            className={styles['nav-content-link']}
+            href={`/${item.toLowerCase()}`}
+          >
+            {item}
+          </Link>
+        </li>
+      ))}
       <li
         className={styles['nav-content']}
-        key={idx}
+        onClick={() => {
+          if (auth.currentUser) {
+            handleSignOut()
+          } else {
+            alert('Error: You are not logged in.')
+          }
+        }}
       >
         <Link
-          className={styles['nav-content-link']}
-          href={`/${item.toLowerCase()}`}
+          className={styles['sign-out-button']}
+          href={''}
         >
-          {item}
+          Sign out
         </Link>
       </li>
-    ))}
-  </>
-)
+    </>
+  )
+}
 
 export default NavContent
